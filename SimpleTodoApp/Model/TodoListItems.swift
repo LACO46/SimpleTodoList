6320d399aa85f7b2471a205bf2f21c89ea95fe3d
Takafumi Tsuzuki <apumk6ke@ezweb.ne.jp>
2020-01-12 22:39:10 +0900

    fix models

---
+++ SimpleTodoApp/Model/TodoListItems.swift
@@ -0,0 +1,30 @@
+//
+//  GetTodoItems.swift
+//  SimpleTodoApp
+//
+//  Created by 都築誉史 on 2020/01/12.
+//  Copyright © 2020 都築誉史. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class TodoListItemModel {
+    var todoItems: Results<TodoItemModel>!
+    
+    func getTodoItems() -> [TodoItemViewModel] {
+        let realm = try! Realm()
+        todoItems = realm.objects(TodoItemModel.self)
+        return todoItems.map{
+            return TodoItemViewModel(id: $0.id, thingsToDo: $0.thingsToDo, priorityName: $0.priorityName, priorityNumber:$0.priorityNumber)
+        }
+    }
+    
+    func deleteTodoItems(id: Int) {
+        let realm = try! Realm()
+        print(todoItems)
+        try! realm.write{
+           realm.delete(todoItems[id])
+        }
+    }
+}
